11c04c326c3b1edca6dca346dc1875ae57fc8d13
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2016-01-15 17:54:09 -0200

    Added Concat and Uglify task
    After transforming CoffeeScript files into js files, gulp will concat and uglify those files and save it in public/js directory as a single scripts.min.js file. 
    A script.js file (without uglify) will be saved in the same folder as well.

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,9 @@ var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var coffee = require('gulp-coffee');
 var gutil = require('gulp-util');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 gulp.task('serve', ['sass'], function () {
 
@@ -17,7 +20,7 @@ gulp.task('serve', ['sass'], function () {
     });
 
     gulp.watch("assets/sass/**/*.scss", ['sass']);
-    gulp.watch("assets/coffee/**/*.coffee", ['coffee']);
+    gulp.watch("assets/coffee/**/*.coffee", ['coffee','js-minify-concat']);
     gulp.watch("js/*.js").on('change', browserSync.reload);
     gulp.watch("*.php").on('change', browserSync.reload);
     gulp.watch("**/*.php").on('change', browserSync.reload);
@@ -31,9 +34,18 @@ gulp.task('sass', function () {
 });
 
 gulp.task('coffee', function () {
-    gulp.src('assets/coffee/*.coffee')
+    return gulp.src('assets/coffee/*.coffee')
         .pipe(coffee({bare: true}).on('error', gutil.log))
-        .pipe(gulp.dest('./public/js/'))
+        .pipe(gulp.dest('assets/js'));
 });
+
+gulp.task('js-minify-concat', function(){
+    return gulp.src('assets/js/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('public/js'))
+        .pipe(rename('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/js'));
+})
 
 gulp.task('default', ['serve']);
